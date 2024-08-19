9d08f4c6769d1bf0233ea8d7414f52117ef34fe5
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2024-08-19 12:52:27 +0900

    Add files via upload

---
+++ P1/ConsoleApp1/P4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("이름을 입력하세요 : ");
+            string name = Console.ReadLine();
+            Console.Write("나이를 입력하세요 : ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("안녕하세요, ");
+            Console.Write(name + "님!");
+            Console.WriteLine("나이는 " + age + "세 입니다.");
+        }
+    }
+}
