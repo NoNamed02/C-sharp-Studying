60731f3ac6215fdf934b152a88a0c16ebeed4d6a
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2024-08-23 23:15:22 +0900

    Add files via upload

---
+++ Part1/P31.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace A031
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("This is a StringBuilder Test");
+            Console.WriteLine($"{sb.ToString()} ({sb.Length} C)");
+
+            sb.Clear();
+            Console.WriteLine($"{sb.ToString()} ({sb.Length} C)");
+
+            sb.Append("new string");
+            Console.WriteLine($"{sb.ToString()} ({sb.Length} C)");
+
+            sb.Insert(5, "xyz", 2);
+            Console.WriteLine($"{sb.ToString()} ({sb.Length} C)");
+
+            sb.Remove(5, 4);
+            Console.WriteLine($"{sb.ToString()} ({sb.Length} C)");
+
+            sb.Replace("xyz", "abc");
+            Console.WriteLine($"{sb.ToString()} ({sb.Length} C)");
+
+            Stopwatch time = new Stopwatch();
+            string test = string.Empty;
+            time.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                test += i;
+            }
+            time.Stop();
+            Console.WriteLine("String : " + time.ElapsedMilliseconds + "ms");
+
+            StringBuilder test1 = new StringBuilder();
+            time.Reset();
+            time.Start();
+            for(int i = 0; i < 100000; i++)
+            {
+                test1.Append(i);
+            }
+            time.Stop();
+            Console.WriteLine("StringB : " + time.ElapsedMilliseconds + "ms");
+        }
+    }
+}
---
+++ Part1/P32.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+
+//열거형
+
+namespace Part1
+{
+    class Program
+    {
+        enum Size
+        {
+            Short, Tall, Grande, Venti
+        };
+        static int[] price = { 3300, 3800, 4300, 4800 };
+        enum Colors
+        {
+            Red = 1, Greem = 2, Blue = 4, yellow = 8
+        };
+        enum Coffee
+        {
+            Short = 3300, Tall = 3800, Grande = 4300, Venti =4800
+        };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("커피 가격표");
+            for(int i = 0; i<4; i++)
+            {
+                if(i == (int)Size.Short)
+                    Console.WriteLine($"{Size.Short, 10} : {price[i]:C}");
+                else if (i == (int)Size.Tall)
+                    Console.WriteLine($"{Size.Tall,10} : {price[i]:C}");
+                else if (i == (int)Size.Grande)
+                    Console.WriteLine($"{Size.Grande,10} : {price[i]:C}");
+                else if (i == (int)Size.Venti)
+                    Console.WriteLine($"{Size.Venti,10} : {price[i]:C}");
+            }
+
+            Console.WriteLine("\n커피 가격표(enum)");
+            foreach(var size in Enum.GetValues(typeof(Size)))
+            {
+                Console.WriteLine($"{size,10} : {price[(int)size]:C}");
+            }
+            Console.WriteLine("\nColors Enum iteration");
+            foreach (var color in Enum.GetValues(typeof(Colors)))
+            {
+                Console.WriteLine($"{color, 10} : {Convert.ToInt32(color)}");
+            }
+
+            Console.WriteLine("\n커피 가격표 Enum");
+            foreach(var coffee in Enum.GetValues(typeof(Coffee)))
+            {
+                Console.WriteLine($"{coffee,10} : {Convert.ToInt32(coffee):C}");
+            }
+        }
+    }
+}
