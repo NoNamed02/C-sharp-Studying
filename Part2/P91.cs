d085581101b2a47b0d9bb6ff8d4083ff165adfab
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2024-08-26 17:01:41 +0900

    Add files via upload

---
+++ Part2/P91.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P91
+{
+    class Program
+    {
+        static int Power(int x, int y = 2)
+        {
+            int result = 1;
+            for (int i = 0; i < y; i++)
+            {
+                result *= x;
+            }
+            return result;
+        }
+        static int Area(int h, int w)
+        {
+            return h * w;
+        }
+
+        static void Print(int a, int b)
+        {
+            Console.WriteLine($"{a}, {b}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Power(1, 2));
+            Console.WriteLine(Power(2));
+
+            Console.WriteLine(Area(2, 3));
+            Console.WriteLine(Area(w:5, h:3));
+
+            Print(b: 3, a: 6);
+        }
+    }
+}
---
+++ Part2/P92.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P92
+{
+    class Program
+    {
+        static void Print(double n)
+        {
+            Console.WriteLine("double " + n);
+        }
+        static void Print(int n)
+        {
+            Console.WriteLine("int " + n);
+        }
+        static void Main(string[] args)
+        {
+            Print(2.0);
+            Print(3);
+        }
+    }
+}
