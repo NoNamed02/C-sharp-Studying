6efc8421d450bce5592812d00222ebee972d3c1f
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2024-08-28 21:13:45 +0900

    Add files via upload

---
+++ Part2/P118.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P118
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> isEven = n => n % 2 == 0;
+            Console.WriteLine(isEven(6));
+
+            Predicate<string> Lower = s => s.Equals(s.ToLower());
+            Console.WriteLine(Lower("Test"));
+        }
+    }
+}
