0fd113e6f2ff561a3bfa64c57868338f3bc654d3
NoNamed02 <128911424+NoNamed02@users.noreply.github.com>
2024-08-28 21:15:44 +0900

    Add files via upload

---
+++ Part2/P96.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace P96
+{
+    class Node
+    {
+        internal int data;
+        internal Node next;
+        public Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+    class LinkedList
+    {
+        Node head;
+        internal void InsertFront(int data)
+        {
+            Node node = new Node(data);
+            node.next = head;
+            head = node;
+        }
+        internal void InsertBack(int data)
+        {
+            Node node = new Node(data);
+            if(head == null)
+            {
+                head = node;
+                return;
+            }
+            Node last = GetLastNode();
+            last.next = node;
+        }
+
+        internal Node GetLastNode()
+        {
+            Node temp = head;
+            while(temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+
+        internal void InsertAfter(int prev, int data)
+        {
+            Node prevNode = null;
+
+            for (Node temp = head; temp != null; temp = temp.next)
+            {
+                if (temp.data == prev)
+                    prevNode = temp;
+                Node node = new Node(data);
+                node.next = prevNode.next;
+                prevNode.next = node;
+            }
+        }
+
+        //등등....
+    }
+}
